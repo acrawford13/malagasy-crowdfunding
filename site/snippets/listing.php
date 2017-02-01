721dd5a34f8129e5dcfb8086842ffdce326905e3
Andrea Crawford <andrea.crawford13@gmail.com>
2017-02-01 17:46:18 +0800

    Set margin-bottom to 0 for listing headings

--- site/snippets/listing.php
+++ site/snippets/listing.php
@@ -1,4 +1,4 @@
-<?php if($data->template()=="text"){if($data->showtitle()){?><h<?= $level ?>><?= $data->title(); ?></h<?= $level ?>><?php }}else{?><h<?= $level ?>><?= $data->title(); ?></h<?= $level ?>><?php } ?>
+<?php if($data->template()=="text"){if($data->showtitle()){?><h<?= $level ?> style="margin-bottom:0"><?= $data->title(); ?></h<?= $level ?>><?php }}else{?><h<?= $level ?> style="margin-bottom:0"><?= $data->title(); ?></h<?= $level ?>><?php } ?>
 
 <?php foreach($data->children()->visible()->filterBy('langhide','!=','1') as $section):
         snippet($section->template(), array('data' => $section, 'level' => 2));

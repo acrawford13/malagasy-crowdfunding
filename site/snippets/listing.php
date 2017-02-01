ad8881942f56257efe589385270054e8a80075d6
Andrea Crawford <andrea.crawford13@gmail.com>
2017-02-01 18:12:04 +0800

    Change listing templates to include line below each level 1 section and change the way sections are handled

--- site/snippets/listing.php
+++ site/snippets/listing.php
@@ -1,5 +1,8 @@
 <?php if($data->template()=="text"){if($data->showtitle()){?><h<?= $level ?> style="margin-bottom:0"><?= $data->title(); ?></h<?= $level ?>><?php }}else{?><h<?= $level ?> style="margin-bottom:0"><?= $data->title(); ?></h<?= $level ?>><?php } ?>
 
-<?php foreach($data->children()->visible()->filterBy('langhide','!=','1') as $section):
-        snippet($section->template(), array('data' => $section, 'level' => 2));
+<?php  $level++;
+
+foreach($data->children()->visible()->filterBy('langhide','!=','1') as $section):
+        snippet($section->template(), array('data' => $section, 'level' => $level));
+        
     endforeach;?>
--- site/templates/listing.php
+++ site/templates/listing.php
@@ -1,9 +1,11 @@
 <?php snippet('header') ?>
 
     <h1 class="page-title"><?= $page->title()->html() ?></h1>
-
+    <?php $count=1; ?>
     <?php foreach($page->children()->visible()->filterBy('langhide','!=','1') as $section):
             snippet($section->template(), array('data' => $section, 'level' => 1));
+            if($count!=count($page->children()->visible()->filterBy('langhide','!=','1'))){echo "<hr>";}
+            $count++;
     endforeach;?>
 
 <?php snippet('footer') ?>

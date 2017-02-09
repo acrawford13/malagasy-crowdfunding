bcba35139cd9f1230e8bc348c1b41824adfeace1
Andrea Crawford <andrea.crawford13@gmail.com>
2017-02-10 00:17:28 +0800

    Split three-column snippet into correct rows

--- site/snippets/threecolumn.php
+++ site/snippets/threecolumn.php
@@ -2,6 +2,8 @@
     <div class="pure-u-1">
         <h<?= $level ?> style="margin-bottom:0"><?= $data->title(); ?></h<?= $level ?>>
     </div>
+</div>
+<div class="pure-g">
     <div class="pure-u-1-1 pure-u-md-<?php if($data->col1width()){echo $data->col1width();}else{echo "1-3";}?> <?php if($data->hidecol1() == '1'){echo "hide-on-mobile";} ?>">
         <?= $data->col1()->kirbytext(); ?>
     </div>

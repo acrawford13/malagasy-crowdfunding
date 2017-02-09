e5e35d8181dd02c1cefeb0077fec3f251eac6f13
Andrea Crawford <andrea.crawford13@gmail.com>
2017-02-10 00:33:14 +0800

    Update contact form

--- site/templates/contactform.php
+++ site/templates/contactform.php
@@ -1,13 +1,36 @@
 <?php snippet('header') ?>
+  <main class="main" role="main">
 
-
+    <header class="wrap">
+      <h1 class="page-title"><?= $page->title()->html() ?></h1>
+    </header>
+    <style>
+    input, textarea {
+        width:100%;
+    }
+    
+    label {
+        color:#ff9800;
+        font-weight:bold;
+        display:inline-block;
+        margin-bottom:0.5rem;
+        margin-top:0.5rem;
+    }
+    
+</style>    
+    <div class="text wrap">
+      <?= $page->text()->kirbytext() ?>
 <form action="<?php echo $page->url() ?>" method="POST">
-    <label>Email</label>
-    <input<?php if ($form->error('email')): ?> class="error"<?php endif; ?> name="email" type="email" value="<?php echo $form->old('email') ?>">
-<?php echo $page->contact(); ?>
-    <label>Name</label>
-    <input<?php if ($form->error('name')): ?> class="error"<?php endif; ?> name="name" type="text" value="<?php echo $form->old('name') ?>">
+      <div class="pure-g">
+      <div class="pure-u-1-1 pure-u-md-1-2">
+          <label>Name</label>
+    <input<?php if ($form->error('name')): ?> class="error"<?php endif; ?> name="name" type="text" value="<?php echo $form->old('name') ?>" placeholder="Name">
+    </div>
+      <div class="pure-u-1-1 pure-u-md-1-2">    <label>Email Address</label>
+    <input<?php if ($form->error('email')): ?> class="error"<?php endif; ?> name="email" type="email" value="<?php echo $form->old('email') ?>" placeholder="your@email.com">
 
+    </div>
+    </div>
     <label>Message</label>
     <textarea<?php if ($form->error('message')): ?> class="error"<?php endif; ?> name="message"><?php echo $form->old('message') ?></textarea>
 
@@ -21,4 +44,7 @@
     <?php snippet('uniform/errors', ['form' => $form]) ?>
 <?php endif; ?>
 
+    </div>
+
+  </main>
 <?php snippet('footer') ?>

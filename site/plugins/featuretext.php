12158ec0d1e98d71f404523d42b0b67aef5ffbd9
Andrea Crawford <andrea.crawford13@gmail.com>
2017-02-11 21:49:04 +0800

    Fix error in Feature Text plugin (replacing spaces with semicolons too often)

--- site/plugins/featuretext.php
+++ site/plugins/featuretext.php
@@ -4,8 +4,8 @@ kirbytext::$post[] = function($kirbytext, $value) {
     $initial = preg_replace('/(\(featuretext.*?)align(.*?\))/','\1text-align\2',$value);
     $initial = preg_replace('/(\(featuretext.*?)\s(size:\w*)\s(.*?\))/','\1 \3 \2',$initial);
     //$initial = preg_replace('/(\(featuretext.*?)size(.*?\))/','\1font-size\2',$initial);
-    $initial = preg_replace('/(color:[^\s)]*)/','\1;',$initial);
-    $initial = preg_replace('/(text-align:[^\s)]*)/','\1;',$initial);
+    $initial = preg_replace('/(color:[^\s)"]*)/','\1;',$initial);
+    $initial = preg_replace('/(text-align:[^\s)"]*)/','\1;',$initial);
     //$initial = preg_replace('/(font-size:[^\s)]*)/','\1;',$initial);
     return preg_replace('/<p>\(featuretext((?:(?:\s?color:[\w\d#]*;?)|(?:\s?text-align:\w*;?)){1,2})\)\s?(?:size:([\w]*))(?:<br \/>)?/','<h2 class="feature-text feature-\2" style="\1">',$initial);
 };

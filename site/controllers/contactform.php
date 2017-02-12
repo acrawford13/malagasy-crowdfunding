e17f08e06557778cc4c234e7f60e099b6969abd9
Andrea Crawford <andrea.crawford13@gmail.com>
2017-02-12 19:53:47 +0800

    Add custom message for contact form

--- site/controllers/contactform.php
+++ site/controllers/contactform.php
@@ -8,6 +8,7 @@ return function ($site, $pages, $page)
     $errors = ['fr'=>['email'=>'L\'adresse email est non valide','message'=>'Vous n\'avez pas renseigné de message'],'mg'=>['email'=>'L\'adresse email est non valide','message'=>'Vous n\'avez pas renseigné de message'],'en'=>['email'=>'Please enter a valid email address','message'=>'Please enter a message']];
 
     $form = new Form([
+        'name' => [],
         'email' => [
             'rules' => ['required', 'email'],
             'message' => $errors[$site->language()->code()]['email']
@@ -21,7 +22,9 @@ return function ($site, $pages, $page)
     if (r::is('POST')) {
         $form->emailAction([
             'to' => $page->contact(),
-            'from' => 'andrea.crawford13@gmail.com'
+            'from' => 'andrea.crawford13@gmail.com',
+            'snippet' => 'emails/success',
+            'subject' => 'New message from Malagasy Crowdfunding website contact form'
         ]);
     }
 
---
+++ site/snippets/emails/success.php
@@ -0,0 +1,6 @@
+Name: <?php echo $data['name'] ?>
+
+Email: <?php echo $data['email'] ?>
+
+
+<?php echo $data['message'] ?>
